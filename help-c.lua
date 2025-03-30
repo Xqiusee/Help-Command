4e54636ca3dee0ed9338102df02a9b5f77f2c09b
Xqiusee <xqiusee@gmail.com>
2025-03-30 16:12:16 +0000

    Update help-c.lua

--- help-c.lua
+++ help-c.lua
@@ -1,3 +1,3 @@
 RegisterCommand('help', function()
-    TriggerEvent('chatMessage', '^2[Server Help]^0', {255, 255, 255}, 'Welcome! Use ^3/rules^0 for server rules and ^3/commands^0 for available commands. ^4/calladmin^0 For any reports, etc.')
+    TriggerEvent('chatMessage', '^1[Server Help]^0', {255, 255, 255}, 'Welcome! Use ^9/rules^0 for server rules and ^6/commands^0 for available commands. ^8/calladmin^0 For any reports, etc.')
 end)
